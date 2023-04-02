33e2a831bb4f5a70e1deb61c1823319c62e561ae
wuqlan111 <wuqlan111@163.com>
2023-04-02 23:52:57 +0800

    Add spi_reg and spi_top.

---
+++ SPI/rtl/spi_top.v
@@ -0,0 +1,47 @@
+
+/****************************************************************************************
+* Module Name:     spi_top
+* Author:          wuqlan
+* Email:           
+* Date Created:    2023/4/2
+* Description:     SPI top module.
+*                  
+*
+* Version:         0.1
+*****************************************************************************************/
+module   spi_top(
+
+                input  clk_in,
+                input  rstn_in,
+
+
+                /*--------spi module top signal-------*/
+                inout  miso_io,
+                inout  mosi_in,
+                inout  sck_in,
+                inout  ss_io
+
+
+                    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
+
+
