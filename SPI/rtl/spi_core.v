399895cb8e575f8e6a08022b06c3ddd0a2d856b8
wuqlan111 <wuqlan111@163.com>
2023-04-02 13:34:09 +0800

    Modify  spi_core.

--- SPI/rtl/spi_core.v
+++ SPI/rtl/spi_core.v
@@ -9,6 +9,23 @@
 *
 * Version:         0.1
 *****************************************************************************************/
+module spi_core (
+
+    /*-------system clk and reset signal*/
+    input  clk_in,
+    input  rstn_in,
+
+    /*------spi signal------*/
+    output   miso_out,
+    input   miso_in,
+    input  mosi_in,
+    output  mosi_out,
+    output  sck_in,
+    input   sck_out,
+    input  ss_in,
+    output  ss_out
+
+);
 
 
 
@@ -20,6 +37,7 @@
 
 
 
+endmodule
 
 
 
