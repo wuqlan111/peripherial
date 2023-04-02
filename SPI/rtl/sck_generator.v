9b7229d6c41f1cd9f296ba9f59eee6cb90dd5f4f
wuqlan111 <wuqlan111@163.com>
2023-04-02 14:56:42 +0800

    Modify sck_generator.

--- SPI/rtl/sck_generator.v
+++ SPI/rtl/sck_generator.v
@@ -10,18 +10,39 @@
 * Version:         0.1
 ***********************************************************************/
 
+module  sck_generator(
+    input   clk_in,
+    input   cpol_in,
+    input   enable_in,
+    input   rstn_in,
+    output  reg  sck_out,
+    input   [2: 0]  sppr_in,
+    input   [2: 0]  spr_in
+);
+
+
+wire  [11: 0]  half_counter;
+wire  change_clk;
+reg   [11: 0]  counter;  
+
+/*----get half counter----*/
+always @(clk_in or rstn_in) begin
+    if (!rstn_in ) begin
+        counter       <=    0;
+        sck_out       <=    cpol_in? 0: 1;
+    end
+    else begin
+        counter       <=    enable_in?  (counter + 1):  0;
+        sck_out       <=    enable_in && change_clk ? ~sck_out:  (cpol_in? 0: 1);
+    end
+end
 
 
 
+assign   half_counter  =   ( sppr_in + 1) << ( spr_in + 1);
+assign   change_clk    =   (counter  ==  half_counter);
 
-
-
-
-
-
-
-
-
+endmodule
 
 
 
