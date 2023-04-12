007794953f2bc175026cf39c5089a6aad2edf620
wuqlan111 <wuqlan111@163.com>
2023-04-12 23:08:21 +0800

    Modify  bclk_generator.

--- UART/rtl/bclk_generator.v
+++ UART/rtl/bclk_generator.v
@@ -14,32 +14,40 @@ module  sck_generator(
     input   clk_in,
     input   enable_in,
     input   rstn_in,
-    output  reg  sck_out,
-    input   [2: 0]  sppr_in,
-    input   [2: 0]  spr_in
+    output  reg  bclk_out,
+    input   [15: 0]  divisor
 );
 
 
+reg   [11: 0]  counter;
+reg   top_half;
+
 wire  [11: 0]  half_counter;
 wire  change_clk;
-reg   [11: 0]  counter;  
+wire  top_half_end;
+wire  next_half_end;
+
 
 /*----get half counter----*/
 always @(clk_in or rstn_in) begin
     if (!rstn_in ) begin
-        counter       <=    0;
-        sck_out       <=    0;
+        counter        <=    0;
+        bclk_out       <=    1;
+        top_half       <=    1;
     end
     else begin
-        counter       <=    enable_in?  (counter + 1):  0;
-        sck_out       <=    enable_in && change_clk ? ~sck_out:  0;
+        counter       <=     enable_in?  (counter + 1):  0;
+        bclk_out      <=     enable_in && change_clk ? ~bclk_out:  bclk_out;
+        top_half      <=     enable_in && change_clk ? ~top_half:  top_half;
     end
 end
 
 
 
-assign   half_counter  =   ( sppr_in + 1) << ( spr_in + 1);
-assign   change_clk    =   (counter  ==  half_counter);
+assign   half_counter  =   divisor >> 1;
+assign   change_clk    =   top_half_end || next_half_end?  1:  0;
+assign   top_half_end  =   top_half && ((half_counter + divisor[0]) == counter) ? 1:  0;
+assign   next_half_end  =  !top_half && (half_counter ==  counter) ? 1:  0;
 
 endmodule
 
